55b93ae5bd3d9949e55693f705e164a301c0d71c
Alex Sin <sinalexandru@gmail.com>
2022-08-21 11:20:31 +0300

    fix login and signup

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -5,20 +5,27 @@ import { supabase } from '../lib/supabaseClient'
 
 export default function SignIn() {
   const router = useRouter()
+  const [loading, setLoading] = useState(false)
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault
-
-    const { error } = await supabase.auth.signInWithPassword({
-      email,
-      password,
-    })
-
-    if (error) {
-      alert(JSON.stringify(error))
-    } else {
+  const handleLogin = async (email: string, password: string) => {
+    try {
+      setLoading(true)
+      // const { error, user } = await supabase.auth.signIn({ email })
+      const { error, data } = await supabase.auth.signInWithPassword({
+        email,
+        password,
+      })
+      if (error) throw error
+      alert('Somehtings up!')
+    } catch (error) {
+      if (error instanceof Error) {
+        console.log('Error thrown:', error.message)
+        alert(error.message)
+      }
+    } finally {
+      setLoading(false)
       router.push('/')
     }
   }
@@ -27,24 +34,30 @@ export default function SignIn() {
     <div className="container" style={{ padding: '50px 0 100px 0' }}>
       <Nav />
       <h1>LOGIN</h1>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
+      <label htmlFor="email">Email</label>
+      <input
+        type="email"
+        id="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
 
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit">SIGN IN</button>
-      </form>
+      <label htmlFor="password">Password</label>
+      <input
+        type="password"
+        id="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button
+        disabled={loading}
+        onClick={(e) => {
+          e.preventDefault()
+          handleLogin(email, password)
+        }}
+      >
+        Login
+      </button>
     </div>
   )
 }
--- pages/signup.tsx
+++ pages/signup.tsx
@@ -5,20 +5,24 @@ import { supabase } from '../lib/supabaseClient'
 
 export default function SignUp() {
   const router = useRouter()
+  const [loading, setLoading] = useState(false)
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault
-
-    const { error } = await supabase.auth.signUp({
-      email,
-      password,
-    })
-
-    if (error) {
-      alert(JSON.stringify(error))
-    } else {
+  const handleRegister = async (email: string, password: string) => {
+    try {
+      setLoading(true)
+      // const { error, user } = await supabase.auth.signIn({ email })
+      const { error, data } = await supabase.auth.signUp({ email, password })
+      if (error) throw error
+      alert('Somehtings up!')
+    } catch (error) {
+      if (error instanceof Error) {
+        console.log('Error thrown:', error.message)
+        alert(error.message)
+      }
+    } finally {
+      setLoading(false)
       router.push('/')
     }
   }
@@ -27,24 +31,30 @@ export default function SignUp() {
     <div className="container" style={{ padding: '50px 0 100px 0' }}>
       <Nav />
       <h1>REGISTER</h1>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
+      <label htmlFor="email">Email</label>
+      <input
+        type="email"
+        id="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
 
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit">SIGNUP</button>
-      </form>
+      <label htmlFor="password">Password</label>
+      <input
+        type="password"
+        id="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button
+        disabled={loading}
+        onClick={(e) => {
+          e.preventDefault()
+          handleRegister(email, password)
+        }}
+      >
+        SIGNUP
+      </button>
     </div>
   )
 }
